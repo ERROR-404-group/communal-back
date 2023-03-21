d22dc26341f961dc3654a8d6843490e62e22e167
Nate Brown <nathan.d.brown@gmail.com>
2023-03-21 13:14:44 -0700

    spotify search returns results, results data converted mapped onto class, returns array of Track objects

--- server.js
+++ server.js
@@ -44,8 +44,14 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 const clientId = process.env.SPOTIFY_API_CLIENT_ID;
 const clientSecret = process.env.SPOTIFY_API_CLIENT_SECRET;
 
+
+// code vomit from SPOTIFY API, uses client id and client secret to generate token
+// token required when sending searches to SPOTIFY API
+
 var client_id = clientId;
 var client_secret = clientSecret;
+
+let spotifyToken;
 
 var authOptions = {
   url: 'https://accounts.spotify.com/api/token',
@@ -59,15 +65,74 @@ var authOptions = {
 };
 
 async function getSpotifyToken() {
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      // var token = body.access_token;
+      var token = body;
+      spotifyToken = body.access_token;
+      console.log(token);
+    }
+  });
+  getSpotifyResults();
+};
 
-request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-    // var token = body.access_token;
-    var token = body;
-    console.log(token);
+// ^^^ end code vomit from SPOTIFY API ^^^
+
+// placeholder location to invoke function to get the token
+getSpotifyToken();
+
+var spotifyRequestHeaders = {
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer BQAXGy3nCB32ASgeCYlQAD4Rgr02lV2NiT0hIopEpCO0UrVKB6PZJKBkUimBlISs4-2BUydgoqht5NDxOncvq5IOLVma2Nb4phelCFeIgnWXws3E1Usv`
   }
-});
+};
 
+var search_string = 'pink floyd animals';
+
+// function to search tracks using Spotify API
+// requires search string (passed from client)
+// search string format: text, separated by spaces is acceptable
+// requires token (generated server side)
+async function getSpotifyResults(search_string) {
+  // local variable for spotify request
+  // let search;
+  // Spotify request URL format
+  // https://api.spotify.com/v1/search?q=<search_string>&type=track&limit=10"
+  // Spotify get request headers
+  // {"Accept: application/json","Content-Type: application/json","Authorization: Bearer <token>"}
+  // try {
+    let search = await axios.get(`https://api.spotify.com/v1/search?q=pink floyd animals&type=track&limit=10`,
+      {headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer BQAXGy3nCB32ASgeCYlQAD4Rgr02lV2NiT0hIopEpCO0UrVKB6PZJKBkUimBlISs4-2BUydgoqht5NDxOncvq5IOLVma2Nb4phelCFeIgnWXws3E1Usv`
+    }});
+    console.log(search.data.tracks.items[1].name);
+    let tracks = search.data.tracks.items.map(
+      trackResult => new Track(trackResult)
+      );
+    console.log(tracks)
+    // return Promise.resolve(search);
+  // } catch (e) {
+  //   search = e.message;
+  //   console.log(search);
+  //   return Promise.reject(search);
+  //   next (e);
+  // }
 }
 
-getSpotifyToken();
+// getSpotifyResults();
+
+let e = {
+  message: 'It looks like I picked the wrong week to quit amphetamines.'
+}
+
+class Track {
+  constructor(TrackObject) {
+    this.album = TrackObject.album.name,
+    this.artist = TrackObject.artists[0].name,
+    this.track = TrackObject.name
+   }
+}
